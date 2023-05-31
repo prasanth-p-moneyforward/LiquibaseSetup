6dab4bfbff05ef079c1bbf46029d027be23b3c04
PrasanthP <penumala.prasanth@moneyforward.co.jp>
2023-05-31 13:27:49 +0900

    Review Comments Fix

--- src/main/java/com/sample/liquibase/App.java
+++ src/main/java/com/sample/liquibase/App.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class App {
     public static void main(String[] args) {
+    	System.out.println("Application Startup!!");
         SpringApplication.run(App.class, args);
     }
 }
