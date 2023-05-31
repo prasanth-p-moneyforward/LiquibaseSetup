fa16ac8a22f6a4b805bca9022c87fbb4a8856bd1
PrasanthP <penumala.prasanth@moneyforward.co.jp>
2023-05-31 13:21:26 +0900

    Updating the file

--- src/main/java/com/sample/liquibase/App.java
+++ src/main/java/com/sample/liquibase/App.java
@@ -3,12 +3,9 @@ package com.sample.liquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.HashSet;
-
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        HashSet hashSet = new HashSet();
         SpringApplication.run(App.class, args);
     }
 }
